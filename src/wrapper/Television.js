60ae1726fc3fa6e2624ec491ba2fea9512dc4b23
Floresmi23 <Floresmi23@outlook.com>
2021-11-08 19:19:54 -0500

    erwt

--- src/wrapper/Television.js
+++ src/wrapper/Television.js
@@ -17,27 +17,19 @@ function Television(){
     return(
         <div style={{backgroundColor: color}}>
             <div>
-                <h1>Name: Michael Scott</h1>
-                <h2>category: television</h2>
-                <h3>value: 3</h3>
+                <h1>Michael Scott Funko</h1>
                 <img src="https://media.gamestop.com/i/gamestop/10173271/POP-Television-The-Office-Michael-Scott?$pdp$" alt="Michael Scott"/>
             </div>
             <div>
-                <h1>Name: Dwight</h1>
-                <h2>category: television</h2>
-                <h3>value: 1</h3>
+                <h1>Dwight Funko</h1>
                 <img src="https://images-na.ssl-images-amazon.com/images/I/518oRvfXItL._AC_SX425_.jpg" alt="Dwight"/>
             </div>
             <div>
-                <h1>Name: Jim</h1>
-                <h2>category: television</h2>
-                <h3>value: 2</h3>
+                <h1>Jim Funko</h1>
                 <img src="https://images-na.ssl-images-amazon.com/images/I/41vdN0vxvrL._AC_.jpg" alt="Jim"/>
             </div>
             <div>
-                <h1>Name: Pam</h1>
-                <h2>category: television</h2>
-                <h3>value: 1</h3>
+                <h1>Pam Funko</h1>
                 <img src="https://images-na.ssl-images-amazon.com/images/I/31R5tyCdCnL._AC_.jpg" alt="Pam"/>
             </div>
             <button onClick={()=>setChange2(!change2)}>Change Color</button>
